228865e771327c2ad07e4670fe67115e3accb08b
nuricovicyyanto <71130472+NuricoVicyyanto@users.noreply.github.com>
2024-11-26 10:19:09 +0800

    initial commit

---
+++ introduction/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 10
+	var y = 20
+	z := 30
+
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+
+	const pi = 3.14
+	fmt.Println("pi:", pi)
+}
